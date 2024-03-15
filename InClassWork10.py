ba23dc3a1a49e314bfdf35291bca62feaf89a659
Quynh-An <vo.quy@northeastern.edu>
2024-03-15 13:04:06 -0400

    Add Course and Student Classes

--- InClassWork10.py
+++ InClassWork10.py
@@ -6,3 +6,18 @@ Created on Fri Mar 15 12:44:02 2024
 @author: tandyllc
 """
 
+class Course:
+    def __init__(self, code, name, num_credits, time):
+        self.code = code
+        self.name = name
+        self.num_credits = num_credits
+        self.time = time
+        self.students_enrolled = []
+        self.prereqs = []
+        
+class Student:
+    def __init__(self, name, student_id, major):
+        self.name = name
+        self.student_id = student_id
+        self.major = major
+        self.course_taken = []

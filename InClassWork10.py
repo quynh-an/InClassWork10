33c52346ff7bd3f69be4d18959f8e3fd6503b50c
Quynh-An <vo.quy@northeastern.edu>
2024-03-15 13:22:50 -0400

    Add registration system for courses

--- InClassWork10.py
+++ InClassWork10.py
@@ -7,13 +7,30 @@ Created on Fri Mar 15 12:44:02 2024
 """
 
 class Course:
-    def __init__(self, code, name, num_credits, time):
+    def __init__(self, code, name, num_credits, time, max_students):
         self.code = code
         self.name = name
         self.num_credits = num_credits
         self.time = time
+        self.max_students = max_students
         self.students_enrolled = []
         self.prereqs = []
+        
+    def add_student(self, student):
+        if len(self.enrolled_students) < self.max_students:
+            print(f"Enrolling {student.name} in {self.name}")
+            for course_code in self.prereqs:
+                print(f"Checking if {student.name} finished {course_code}")
+                for finished_course in student.finished_courses:
+                    if finished_course.code == course_code:
+                        self.enrolled_students.append(student.name)
+                        print(f"{student.name} meets the prerequisites for {self.name}.")
+            if self.prereqs == []:
+                self.enrolled_students.append(student.name)
+
+            return True
+        else:
+            return False
         
 class Student:
     def __init__(self, name, student_id, major):
@@ -21,3 +38,4 @@ class Student:
         self.student_id = student_id
         self.major = major
         self.course_taken = []
+        

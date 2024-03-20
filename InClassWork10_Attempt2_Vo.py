6cf0f10177c8b724f819def9610b179632e4fb8c
Quynh-An <vo.quy@northeastern.edu>
2024-03-20 12:32:23 -0400

    Fix dual majors'

--- InClassWork10_Attempt2_Vo.py
+++ InClassWork10_Attempt2_Vo.py
@@ -12,6 +12,7 @@ class Student():
         self.student_id = student_id
         self.course_taken = []
         self.enrolled_courses = []
+        credit_limit = 16
         
     def enroll(self, course):
         if course.add_student(self):
@@ -27,23 +28,69 @@ class Student():
     def get_enrolled_courses(self):
         return self.enrolled_courses
 
-class ElectricalEngineering(Student):
+class Major(Student):
+    def __init__(self, name, student_id):
+        super().__init__(name, student_id)
+        elective_courses = ["EECE2540", "EECE3410", "PHYS4623", "PHYS4115"]
+        pass
+
+class ElectricalEngineering(Major):
     def __init__(self, name, student_id):
         super().__init__(name, student_id)
         self.required_courses = ["EECE2140", "PHYS1151"]
 
-
-class Physics(Student):
+class Physics(Major):
     def __init__(self, name, student_id):
         super().__init__(name, student_id)
-        self.required_courses = ["PHYS1151", "ENCP3000"]
-
+        self.required_courses = ["PHYS1151", "PHYS2303"]
 
 class ElectricalEngineering_and_Physics(ElectricalEngineering, Physics):
     def __init__(self, name, student_id):
         super().__init__(name, student_id)
-        self.required_courses = list(set(ElectricalEngineering.required_courses + Physics.required_courses))
+        EE = ElectricalEngineering(name, student_id)
+        physics_major = Physics(name, student_id)
+        self.required_courses = list(set(EE.required_courses + physics_major.required_courses))
         
+class Course:
+    def __init__(self, code, name, num_credits, day, time, max_students):
+        self.code = code
+        self.name = name
+        self.num_credits = num_credits
+        self.time = time
+        self.max_students = max_students
+        self.enrolled_students = []
+        self.prereqs = []
+        
+    def add_student(self, student):
+        if len(self.enrolled_students) < self.max_students:
+            print(f"Enrolling {student.name} in {self.name}")
+            for course_code in self.prereqs:
+                print(f"Checking if {student.name} finished {course_code}")
+                for finished_course in student.finished_courses:
+                    if finished_course.code == course_code:
+                        self.enrolled_students.append(student.name)
+                        print(f"{student.name} meets the prerequisites for {self.name}.")
+            if self.prereqs == []:
+                self.enrolled_students.append(student.name)
+            return True
+        else:
+            return False
+    
+    def is_full(self):
+        return len(self.enrolled_students) >= self.max_students
+    
+    def add_prerequisites(self, prereq_course_code):
+        self.prereqs.append(prereq_course_code.code)
+        
+    def get_prerequisites(self):
+        return self.prereqs
+    
+    def get_enrolled_students(self):
+        return self.enrolled_students
+
+
+
 # =============================
 rachel = ElectricalEngineering_and_Physics("Rachel Smith", 10001)
+PHYS4623 = Course("PHYS4623", "Medical Physics", 4, "Wednesday", "11:45", 25)
 print (rachel.required_courses)

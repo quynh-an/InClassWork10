882915f564ec33683fd2e317bc135f77d761e755
Quynh-An <vo.quy@northeastern.edu>
2024-03-20 11:06:01 -0400

    Add student with dual major, single inheritance

--- InClassWork10.py
+++ InClassWork10.py
@@ -6,11 +6,12 @@ Created on Fri Mar 15 12:44:02 2024
 @author: tandyllc
 """
 
-class Student:
+class Student():
     def __init__(self, name, student_id, major):
         self.name = name
         self.student_id = student_id
         self.course_taken = []
+        self.major = major
         self.enrolled_courses = []
         
     def enroll(self, course):
@@ -34,16 +35,19 @@ class DualMajorStudent(Student):
         
     def enroll(self, course):
         if course.major_requirement == self.major or course.major_requirement == self.major2:
-            self.courses.append(course)
-            return True
+            if course.add_student(self):
+                self.enrolled_courses.append(course.code)
+                print(f"{self.name} successfully enrolled in {course.name}")
+            else:
+                print(f"Failed to enroll {self.name} in {course.name}: Course full.")
         else:
-            return False
+            print(f"Failed to enroll {self.name} in {course.name}: Course major requirement not met.")
         
 class Course:
     def __init__(self, code, name, major_requirement, num_credits, day, time, max_students):
         self.code = code
         self.name = name
-        self,major_requirement = major_requirement
+        self.major_requirement = major_requirement
         self.num_credits = num_credits
         self.time = time
         self.max_students = max_students
@@ -80,9 +84,18 @@ class Course:
 # ==============================================================
 def main():
     EECE2140 = Course("EECE2140", "Computing Fundamentals for Engineers", "Electrical Engineering", 4, "Wednesday", "11:45", 35)
+    PHYS1151 = Course("PHYS1151", "Physics 1 for Engineers", "Physics", 4, "Monday", "11:45", 40) 
     jane = Student("Jane Doe", 10001, "Electrical Engineering")
     jane.enroll(EECE2140)
-    print(EECE2140.get_enrolled_students())
     print(jane.get_enrolled_courses())
+    rachel = DualMajorStudent("Rachel Smith", 10002, "Electrical Engineering", "Physics")
+    rachel.enroll(PHYS1151)
+    rachel.enroll(EECE2140)
+    print(PHYS1151.get_enrolled_students())
+    print(rachel.get_enrolled_courses())
+    print(EECE2140.get_enrolled_students())
+
+    
+    
     
 main()
